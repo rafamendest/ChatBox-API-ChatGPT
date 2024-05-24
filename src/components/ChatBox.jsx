759239c30f5c0a12fb1ef57abdebe2a23f6b3250
rteixeira <rteixeira@cpqd.com.br>
2024-05-24 10:10:35 -0300

    Ajustes

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,38 +1,49 @@
 import React from "react";
 import { useState } from "react";
-import { ChatGPT } from "../services/apis.js";
+import OpenAI from "openai";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const ChatBox = () => {
   const [data, setData] = useState("");
+  const [optionSelect, setOptionSelect] = useState("textGeneration");
   const [loading, setLoading] = useState(false);
+  const openai = new OpenAI({
+    apiKey: process.env.REACT_APP_CHATGPT_KEY,
+    dangerouslyAllowBrowser: true,
+  });
+  const textGeneration = (e) => {
+    return {
+      model: "gpt-3.5-turbo-instruct",
+      prompt: e.target.value,
+      max_tokens: 1000,
+    };
+  };
+  const imageGeneration = (e) => {
+    return {
+      model: "dall-e-3",
+      prompt: e.target.value,
+      n: 1,
+      size: "1024x1024",
+    };
+  };
 
   const handleSubmitQuestion = async (e) => {
     if (e.keyCode === 13 && e.shiftKey === false) {
       setLoading(true);
-
-      // const params = {
-      //   model: "text-davinci-003",
-      //   prompt: e.target.value,
-      //   max_tokens: 56,
-      //   temperature: 0,
-      // };
-
-      const params = {
-        prompt: e.target.value,
-        n: 2,
-        size: "1024x1024",
-      };
-
       try {
-        // const response = await ChatGPT.post("https://api.openai.com/v1/completions", params);
-        const response = await ChatGPT.post(
-          "https://api.openai.com/v1/images/generations",
-          params
-        );
-
-        window.open(response.data.data[0].url, '_blank');
-        
-        setData(response.data.choices[0].text);
+        if (optionSelect === "textGeneration") {
+          const completion = await openai.completions.create(textGeneration(e));
+          setData(completion.choices[0].text);
+          return;
+        }
+        const response = await openai.images.generate(imageGeneration(e));
+        window.open(response.data[0].url, "_blank");
+        setData("");
       } catch (err) {
         console.error(err);
       } finally {
@@ -49,13 +60,45 @@ const ChatBox = () => {
         id="answer-box"
         value={loading ? "Carregando..." : data}
       />
-      
-      <textarea
-        className="text-box"
-        id="text-box"
-        placeholder="Escreva alguma coisa..."
-        onKeyDown={(e) => handleSubmitQuestion(e)}
-      />
+
+      <Box display="flex" width="80%" margin="auto">
+        <textarea
+          className="text-box"
+          id="text-box"
+          placeholder="Escreva alguma coisa..."
+          onKeyDown={(e) => handleSubmitQuestion(e)}
+        />
+        <Box marginLeft="100px" width="220px">
+          <FormControl>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="textGeneration"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel
+                value="textGeneration"
+                control={<Radio />}
+                label={
+                  <Typography variant="body1" style={{ fontSize: "3rem" }}>
+                    Texto
+                  </Typography>
+                }
+                onChange={(e) => setOptionSelect(e.target.value)}
+              />
+              <FormControlLabel
+                value="imageGeneration"
+                control={<Radio />}
+                label={
+                  <Typography variant="body1" style={{ fontSize: "3rem" }}>
+                    Imagem
+                  </Typography>
+                }
+                onChange={(e) => setOptionSelect(e.target.value)}
+              />
+            </RadioGroup>
+          </FormControl>
+        </Box>
+      </Box>
     </>
   );
 };
--- src/services/apis.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const ChatGPT = axios.create({
-  headers: { Authorization: `Bearer ${process.env.REACT_APP_CHATGPT_KEY}` },
-});
-
